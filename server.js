a363c92346ba1f2793bd69c848be9da8d7e43951
omar adam <omar_adam1000@hotmail.com>
2021-02-17 13:02:09 +0200

    build production env

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express =require("express");
-const app = express();
 const connectDB = require("./config/db");
+const path = require('path');
 
+const app = express();
 //connecting database
 connectDB();
 
 //initalize middleware
 app.use(express.json({extended:false}));
 
-app.get("/",(req,res)=>{
-    res.send("API running succesfully");
-})
+
 
 //define routes 
 app.use("/api/users",require('./routes/api/users'));
@@ -18,7 +17,15 @@ app.use("/api/auth",require('./routes/api/auth'));
 app.use("/api/posts",require('./routes/api/posts'));
 app.use("/api/profile",require('./routes/api/profile'));
 
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port ,()=> console.log(`server starts at port ${port}`));
